66b42aca7d3d6bf57977aa3b1eedf6d213ad2861
pbentes <pedrogomes2000.3@gmail.com>
2024-05-16 20:50:14 +0100

    Added a function to create a middleware stack

--- middleware/common.go
+++ middleware/common.go
@@ -1,3 +1,17 @@
 package middleware
 
+import "net/http"
+
 type Key string
+
+type Middleware func(next http.Handler, arguments *map[string]interface{}) http.Handler
+
+func CreateStack(arguments *map[string]interface{}, middlewares ...Middleware) Middleware {
+	return func(next http.Handler, arguments *map[string]interface{}) http.Handler {
+		for i := len(middlewares); i >= 0; i-- {
+			middleware := middlewares[i]
+			next = middleware(next, arguments)
+		}
+		return next
+	}
+}
