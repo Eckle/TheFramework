2b238cd60a6e1aefa6717da84f822949316ad339
pbentes <pedrogomes2000.3@gmail.com>
2024-05-17 23:44:47 +0100

    Added support for multiple database drivers in the db module

--- db/db.go
+++ db/db.go
@@ -3,14 +3,25 @@ package db
 import (
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 )
 
 var Database *sqlx.DB = nil
 
-func InitDB() {
-	db, err := sqlx.Open("libsql", os.Getenv("DATABASE_URL"))
+type DatabaseDriver string
+
+const (
+	Postgres DatabaseDriver = "postgres"
+	MySQL    DatabaseDriver = "mysql"
+	Turso    DatabaseDriver = "libsql"
+)
+
+func InitDB(driver DatabaseDriver) {
+	db, err := sqlx.Open(string(driver), os.Getenv("DATABASE_URL"))
+
 	if err != nil {
 		panic(err)
 	}
