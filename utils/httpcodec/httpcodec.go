140596a1fbdf5c3801770224674b4fed0b8bbc93
pbentes <pedrogomes2000.3@gmail.com>
2024-08-04 22:18:39 +0100

    Added http codec

---
+++ utils/httpcodec/httpcodec.go
@@ -0,0 +1,35 @@
+package httpcodec
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+func Encode(w http.ResponseWriter, data interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+
+	err := json.NewEncoder(w).Encode(data)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Decode(r *http.Request) (map[string]interface{}, error) {
+	content, err := io.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make(map[string]interface{})
+
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		return nil, errors.New("could not parse request body")
+	}
+
+	return data, nil
+}
